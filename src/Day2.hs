6542927f33cebb2ffe0c69a6fe25ced22c666f8b
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2022-12-01 19:34:30 -0800

    scaffold day 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,13 +1,22 @@
 module Day2 (part1, part2, input) where
 
 import Data.Text (Text)
-import Lib (fetch)
+import Lib (Parser, doParse, fetch)
+
+day :: Integer
+day = 2
 
 input :: Text
-input = fetch 2
+input = fetch day
+
+pInput :: Parser a
+pInput = undefined
+
+parse :: Text -> a
+parse = doParse pInput
 
 part1 :: Text -> Integer
-part1 = undefined
+part1 = undefined . parse
 
 part2 :: Text -> Integer
-part2 = undefined
+part2 = undefined . parse
