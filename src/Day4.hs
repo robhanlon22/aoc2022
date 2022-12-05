00a13a2394086ee5d4473d9a7aa80154a7e9720f
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2022-12-05 09:52:48 -0800

    day 4 refactor for some reason

--- src/Day4.hs
+++ src/Day4.hs
@@ -49,30 +49,23 @@ parse = doParse pInput
 newUnions :: Pair -> Unions
 newUnions pair@(Pair {..}) = let unions = [lhs] `union` [rhs] in Unions {..}
 
+containing :: (Pair -> Range Integer -> Bool) -> Unions -> Bool
+containing f u@(Unions {..}) = check unions
+  where
+    check [] = False
+    check [i] = f pair i
+    check _ = False
+
 -- Solves the puzzle! f is a function that will be used to filter a list of
 -- Unions. The solution is the number of Unions that pass the test.
-solve :: (Unions -> Bool) -> Text -> Int
-solve f = countBy f . map newUnions . parse
+solve :: (Pair -> Range Integer -> Bool) -> Text -> Int
+solve f = countBy (containing f) . map newUnions . parse
 
 -- A Unions is considered encompassing if its unions list is a continuous and its
 -- only element is equal to either Range it its Pair.
-part1' :: Unions -> Bool
-part1' (Unions {pair = Pair {..}, ..}) = isEncompassing unions
-  where
-    isEncompassing [] = False
-    isEncompassing [i] = i == lhs || i == rhs
-    isEncompassing _ = False
-
 part1 :: Text -> Int
-part1 = solve part1'
+part1 = solve (\Pair {..} i -> i == lhs || i == rhs)
 
 -- A Unions is considered overlapping if its unions list is a continuous.
-part2' :: Unions -> Bool
-part2' (Unions {..}) = isOverlapping unions
-  where
-    isOverlapping [] = False
-    isOverlapping [_] = True
-    isOverlapping _ = False
-
 part2 :: Text -> Int
-part2 = solve part2'
+part2 = solve (\_ _ -> True)
