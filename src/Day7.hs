e0816f2b952bbcac5338b6c227a63d52e26d576f
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2022-12-08 09:23:10 -0800

    8

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,36 +1,55 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Day7 (part1Sample, part1Input, part2Sample, part2Input, input, sample) where
 
 import Control.Monad (void)
-import Data.Maybe (catMaybes, mapMaybe)
-import qualified Data.Text as T
+import Data.HashMap.Lazy qualified as HM
+import Data.Sequence qualified as S
+import Data.Text qualified as T
 import Lib (Parser, fetch, solve)
 import Text.Megaparsec
-  ( MonadParsec (eof, lookAhead),
-    choice,
-    many,
-    manyTill,
-    (<|>),
-  )
 import Text.Megaparsec.Char (char, newline, printChar, string)
-import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Megaparsec.Char.Lexer qualified as L
+import Text.Pretty.Simple
 
--- import qualified Data.HashMap.Lazy as HM
-
-type Result = Int
+type Result = Integer
 
 day :: Integer
 day = 7
 
--- type Filesystem = HM.HashMap T.Text (HM.HashMap T.Text Child)
+data Command
+  = CdCommand CdDirectory
+  | LsCommand
+  deriving (Eq, Show)
 
-data Size = Unknown | Known Int deriving (Eq, Show)
+data CdDirectory = CdRelative T.Text | CdRoot | CdParent
+  deriving (Eq, Show)
 
-data Child = Subdirectory Directory | File T.Text Int deriving (Eq, Show)
+data Output
+  = DirectoryOutput T.Text
+  | FileOutput Integer T.Text
+  deriving (Eq, Show)
 
-data Directory = Directory T.Text [Child] Size deriving (Eq, Show)
+data TerminalLine
+  = CommandTerminalLine Command
+  | OutputTerminalLine Output
+  deriving (Eq, Show)
+
+newtype World = World [TerminalLine]
+  deriving (Eq, Show)
+
+newtype FileNode = FileNode Integer
+  deriving (Eq, Show)
+
+newtype DirectoryNode = DirectoryNode (HM.HashMap T.Text FsNode)
+  deriving (Eq, Show)
+
+data FsNode
+  = DirectoryFsNode DirectoryNode
+  | FileFsNode FileNode
+  deriving (Eq, Show)
 
 input :: T.Text
 input = fetch day
@@ -39,114 +58,158 @@ sample :: T.Text
 sample = "$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k\n"
 
 pName :: Parser T.Text
-pName = T.pack <$> manyTill printChar newline
+pName = T.pack <$> manyTill printChar (lookAhead newline)
 
-pCdParent :: Parser ()
-pCdParent = do
-  void $ string "$ cd .."
-  void newline
+tryChoice :: [Parser a] -> Parser a
+tryChoice ps = choice (map try ps)
 
-pCdDir :: Parser Directory
-pCdDir = do
-  void $ string "$ cd "
-  name <- pName
-  children <- manyTill pChildren (pCdParent <|> eof)
-  return $ Directory name (concat children) Unknown
+pCdCommand :: Parser Command
+pCdCommand =
+  commandParser
+    "cd "
+    ( CdCommand
+        <$> choice
+          [ CdRoot <$ char '/',
+            CdParent <$ string "..",
+            CdRelative <$> pName
+          ]
+    )
 
-pFile :: Parser Child
-pFile = do
-  size <- L.decimal
-  void $ char ' '
-  filename <- pName
-  return $ File filename size
+pLsCommand :: Parser Command
+pLsCommand = commandParser "ls" (return LsCommand)
 
-pIgnoreDir :: Parser ()
-pIgnoreDir = do
-  void $ string "dir "
-  void pName
+commandParser :: T.Text -> Parser Command -> Parser Command
+commandParser command parser = do
+  void $ string "$ "
+  void $ string command
+  parser
 
-pLsOutput :: Parser (Maybe Child)
-pLsOutput =
-  choice
-    [ Just <$> pFile,
-      Nothing <$ pIgnoreDir
+pCommand :: Parser Command
+pCommand =
+  tryChoice
+    [ pCdCommand,
+      pLsCommand
     ]
 
-pLs :: Parser [Child]
-pLs = do
-  void $ string "$ ls"
-  void newline
-  children <- manyTill pLsOutput (void (lookAhead $ char '$') <|> eof)
-  return $ catMaybes children
+pCommandTerminalLine :: Parser TerminalLine
+pCommandTerminalLine = CommandTerminalLine <$> pCommand
 
-pSubdir :: Parser [Child]
-pSubdir = do
-  dir <- pCdDir
-  return [Subdirectory dir]
+pFileOutput :: Parser Output
+pFileOutput = do
+  size <- L.decimal
+  void $ char ' '
+  FileOutput size <$> pName
 
-pChildren :: Parser [Child]
-pChildren = pSubdir <|> pLs
+pDirectoryOutput :: Parser Output
+pDirectoryOutput = do
+  void $ string "dir "
+  DirectoryOutput <$> pName
 
-pInput :: Parser Directory
-pInput = do
-  void $ string "$ cd /"
-  void newline
-  children <- many pChildren
-  return $ Directory "/" (concat children) Unknown
+pOutput :: Parser Output
+pOutput = tryChoice [pFileOutput, pDirectoryOutput]
 
-calculateSizes :: Directory -> Directory
-calculateSizes (Directory name entries _) = Directory name entries' (Known size)
+pOutputTerminalLine :: Parser TerminalLine
+pOutputTerminalLine = OutputTerminalLine <$> pOutput
+
+pTerminalLine :: Parser TerminalLine
+pTerminalLine = tryChoice [pCommandTerminalLine, pOutputTerminalLine]
+
+pWorld :: Parser World
+pWorld = World <$> pTerminalLine `endBy` newline
+
+newFs :: DirectoryNode
+newFs = DirectoryNode HM.empty
+
+addNode :: DirectoryNode -> FsNode -> T.Text -> S.Seq T.Text -> DirectoryNode
+addNode fs@(DirectoryNode children) node name path =
+  let insertChild k v = HM.insert k v children
+   in case S.viewl path of
+        S.EmptyL ->
+          let updated = DirectoryNode $ insertChild name node
+           in case node of
+                DirectoryFsNode _ ->
+                  case children HM.!? name of
+                    Just (DirectoryFsNode _) -> fs
+                    _ -> updated
+                _ -> updated
+        p S.:< ps ->
+          let child = case children HM.!? p of
+                Just (DirectoryFsNode d) -> d
+                _ -> newFs
+              directory = addNode child node name ps
+           in DirectoryNode $ insertChild p (DirectoryFsNode directory)
+
+buildFs :: World -> DirectoryNode
+buildFs (World terminalLines) = build terminalLines newFs S.empty
   where
-    calc (Subdirectory d) = Subdirectory $ calculateSizes d
-    calc e = e
-    entries' = map calc entries
-    size =
-      sum $
-        map
-          ( \case
-              Subdirectory (Directory _ _ (Known s)) -> s
-              Subdirectory (Directory _ _ Unknown) -> error "oops"
-              File _ s -> s
-          )
-          entries'
+    build [] fs _ = fs
+    build (tl : tls) fs@(DirectoryNode _) path =
+      let addDirectoryNode name = addNode fs (DirectoryFsNode newFs) name path
+       in case tl of
+            CommandTerminalLine command ->
+              case command of
+                LsCommand -> build tls fs path
+                CdCommand directory ->
+                  case directory of
+                    CdRoot ->
+                      build tls fs S.empty
+                    CdParent ->
+                      build
+                        tls
+                        fs
+                        ( case S.viewr path of
+                            (xs S.:> _) -> xs
+                            _ -> path
+                        )
+                    CdRelative name ->
+                      build tls (addDirectoryNode name) (path S.|> name)
+            OutputTerminalLine output ->
+              case output of
+                DirectoryOutput name ->
+                  build tls (addDirectoryNode name) path
+                FileOutput size name ->
+                  let fs' = addNode fs (FileFsNode $ FileNode size) name path
+                   in build tls fs' path
 
-collectDirectories :: Directory -> [Directory]
-collectDirectories dir@(Directory _ entries _) = dir : concatMap collect entries
-  where
-    collect (Subdirectory d) = collectDirectories d
-    collect _ = []
+data DirectorySize = DirectorySize Integer [DirectorySize] deriving (Eq, Show)
 
-part1 :: Directory -> Result
-part1 i =
-  sum
-    $ mapMaybe
-      ( \case
-          (Directory _ _ (Known s)) -> if s <= 100000 then Just s else Nothing
-          _ -> Nothing
-      )
-    $ collectDirectories
-    $ calculateSizes i
+calculateSizes :: DirectoryNode -> DirectorySize
+calculateSizes (DirectoryNode children) =
+  foldl
+    ( \(DirectorySize size childSizes) child ->
+        case child of
+          DirectoryFsNode directory ->
+            let childSize@(DirectorySize size' _) = calculateSizes directory
+             in DirectorySize (size + size') (childSize : childSizes)
+          FileFsNode (FileNode size') ->
+            DirectorySize (size + size') childSizes
+    )
+    (DirectorySize 0 [])
+    children
 
-part2 :: Directory -> Result
+flattenSizes :: DirectorySize -> [Integer]
+flattenSizes (DirectorySize size children) = size : concatMap flattenSizes children
+
+part1 :: World -> Result
+part1 =
+  sum . filter (<= 100000) . flattenSizes . calculateSizes . buildFs
+
+space :: Integer
+space = 70000000
+
+needed :: Integer
+needed = 30000000
+
+part2 :: World -> Result
 part2 i =
-  let dirs = collectDirectories $ calculateSizes i
-      rootSize = case head dirs of
-        Directory _ _ (Known s) -> s
-        _ -> error "ack"
-      space = 70000000
-      used = space - rootSize
-      needed = 30000000
-   in minimum
-        $ mapMaybe
-          ( \case
-              (Directory _ _ (Known s)) ->
-                if used + s >= needed then Just s else Nothing
-              _ -> Nothing
-          )
-        $ tail dirs
+  let (DirectorySize size children) = calculateSizes $ buildFs i
+      used = space - size
+   in minimum $
+        filter (\s -> used + s >= needed) $
+          concatMap flattenSizes children
 
-solve' :: (Directory -> Result) -> T.Text -> Result
-solve' part = solve part pInput
+solve' :: (World -> Result) -> T.Text -> Result
+solve' part = solve part pWorld
 
 part1Sample :: Result
 part1Sample = solve' part1 sample
--- src/Day8.hs
+++ src/Day8.hs
@@ -1,21 +1,17 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
-
--- {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 
 module Day8 (part1Sample, part1Input, part2Sample, part2Input, input, sample) where
 
-import qualified Data.Text as T
-import Lib
+import Data.Text qualified as T
+import Data.Vector qualified as V
+import Lib (Parser, fetch, solve)
+import Text.Megaparsec (MonadParsec (lookAhead), endBy, manyTill)
+import Text.Megaparsec.Char (digitChar, newline)
 
--- import qualified Data.HashSet as HS
--- import qualified Data.Vector as V
--- import qualified Data.HashMap.Lazy as HM
--- import Text.Megaparsec
--- import Text.Megaparsec.Char
--- import qualified Text.Megaparsec.Char.Lexer as L
--- import Data.List.Split
-
-data Input = Input {}
+type World = V.Vector (V.Vector Integer)
 
 type Result = Int
 
@@ -26,20 +22,92 @@ input :: T.Text
 input = fetch day
 
 sample :: T.Text
-sample = ""
+sample = "30373\n25512\n65332\n33549\n35390\n"
 
-pInput :: Parser Input
-pInput = do
-  return Input {}
+pDigit :: Parser Integer
+pDigit = do
+  digit <- digitChar
+  return (read [digit] :: Integer)
 
-part1 :: Input -> Result
-part1 _i = undefined
+pDigits :: Parser (V.Vector Integer)
+pDigits = V.fromList <$> manyTill pDigit (lookAhead newline)
 
-part2 :: Input -> Result
-part2 _i = undefined
+pWorld :: Parser World
+pWorld = V.fromList <$> (pDigits `endBy` newline)
 
-solve' :: (Input -> Result) -> T.Text -> Result
-solve' part = solve part pInput
+data Cell = Cell {value :: Integer, coord :: (Int, Int)} deriving (Eq, Show)
+
+instance Ord Cell where
+  compare Cell {value = a} Cell {value = b} = a `compare` b
+
+data Surroundings = Surroundings
+  { cell :: Cell,
+    rays :: [[Cell]]
+  }
+  deriving (Eq, Show)
+
+surroundings :: V.Vector (V.Vector Integer) -> [Surroundings]
+surroundings i =
+  let height = V.length i
+      width = V.length $ i V.! 0
+      indices =
+        map
+          (\row -> map (row,) [0 .. (pred width)])
+          [0 .. (pred height)]
+      makeCell coord@(row, col) = Cell {value = i V.! row V.! col, ..}
+   in concatMap
+        ( map
+            ( \coord@(row, col) ->
+                let left =
+                      [ makeCell (row, c)
+                        | c <- [(pred col), (pred (pred col)) .. 0]
+                      ]
+                    right =
+                      [ makeCell (row, c)
+                        | c <- [(succ col) .. (pred width)]
+                      ]
+                    up =
+                      [ makeCell (r, col)
+                        | r <- [(pred row), (pred (pred row)) .. 0]
+                      ]
+                    down =
+                      [ makeCell (r, col)
+                        | r <- [(succ row) .. (pred height)]
+                      ]
+                 in Surroundings
+                      { cell = makeCell coord,
+                        rays = filter (not . null) [left, right, up, down]
+                      }
+            )
+        )
+        indices
+
+part1 :: World -> Result
+part1 =
+  sum
+    . map
+      (\Surroundings {..} -> if any (all (cell >)) rays then 1 else 0)
+    . surroundings
+
+part2 :: World -> Result
+part2 =
+  maximum
+    . map
+      ( \Surroundings {..} ->
+          product
+            ( map
+                ( \ray ->
+                    min
+                      (length (takeWhile (cell >) ray) + 1)
+                      (length ray)
+                )
+                rays
+            )
+      )
+    . surroundings
+
+solve' :: (World -> Result) -> T.Text -> Result
+solve' part = solve part pWorld
 
 part1Sample :: Result
 part1Sample = solve' part1 sample
@@ -51,4 +119,4 @@ part2Sample :: Result
 part2Sample = solve' part2 sample
 
 part2Input :: Result
-part2Input = solve' part2 sample
+part2Input = solve' part2 input
