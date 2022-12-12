1ad08c093b60e9895359a605d2bed9cae783e1a2
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2022-12-12 00:06:02 -0800

    remove node type

--- src/Day12.hs
+++ src/Day12.hs
@@ -4,7 +4,6 @@ import Algorithm.Search
 import Data.Text qualified as T
 import Lib
 import RIO
-import RIO.Char
 import RIO.HashMap qualified as HM
 import RIO.List
 import RIO.Partial
@@ -58,7 +57,7 @@ data Mark
 type Result = Maybe Int
 
 canStepTo :: Mark -> Mark -> Bool
-canStepTo l r = r < l || l == r || (l /= End && succ l == r)
+canStepTo l r = r < l || l == r || succ l == r
 
 pMark :: Parser Mark
 pMark =
@@ -99,12 +98,6 @@ pMarks = manyTill pMark (lookAhead newline)
 pInput :: Parser Input
 pInput = pMarks `endBy` newline
 
-data Node = Node
-  { mark :: Mark,
-    neighbors :: [(Int, Int)]
-  }
-  deriving (Eq, Show)
-
 deltas :: [(Int, Int)]
 deltas = [(dx, dy) | dx <- [-1 .. 1], dy <- [-1 .. 1]]
 
@@ -117,7 +110,7 @@ neighborCoords (x, y) =
 manhattan :: Num a => (a, a) -> (a, a) -> a
 manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
-structures :: [[Mark]] -> ([((Int, Int), Mark)], HashMap (Int, Int) Node)
+structures :: [[Mark]] -> ([((Int, Int), Mark)], HashMap (Int, Int) [(Int, Int)])
 structures ipt =
   let tuples =
         concat $
@@ -134,29 +127,23 @@ structures ipt =
       graph =
         HM.mapWithKey
           ( \k mark ->
-              let neighbors =
-                    mapMaybe
-                      ( \c -> do
-                          v <- HM.lookup c m
-                          guard $ mark `canStepTo` v
-                          return c
-                      )
-                      (neighborCoords k)
-               in Node {mark = mark, neighbors = neighbors}
+              mapMaybe
+                ( \c -> do
+                    v <- HM.lookup c m
+                    guard $ mark `canStepTo` v
+                    return c
+                )
+                (neighborCoords k)
           )
           m
    in (tuples, graph)
 
-minCost :: HashMap (Int, Int) Node -> (Int, Int) -> (Int, Int) -> Maybe Int
+minCost :: HashMap (Int, Int) [(Int, Int)] -> (Int, Int) -> (Int, Int) -> Maybe Int
 minCost graph start end = do
   (cost, _) <-
-    aStar
-      ( \k -> case HM.lookup k graph of
-          Just Node {neighbors} -> neighbors
-          Nothing -> []
-      )
+    dijkstra
+      (\k -> fromMaybe [] $ HM.lookup k graph)
       manhattan
-      (manhattan end)
       (== end)
       start
   return cost
