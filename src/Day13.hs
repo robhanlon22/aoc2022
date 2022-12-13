89be7d33de644204069a873b2d758a9c0e200f78
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2022-12-12 23:29:33 -0800

    Use typeclass

--- src/Day13.hs
+++ src/Day13.hs
@@ -27,25 +27,39 @@ input = fetchSafe day
 sample :: IO T.Text
 sample = return "[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]\n"
 
-type Input = [([Packet], [Packet])]
+type Input = [(Packet, Packet)]
 
 type Result = Maybe Int
 
 data Packet = Literal Int | Packet [Packet] deriving (Eq, Show)
 
-pPacket :: Parser [Packet]
-pPacket =
-  between
-    (char '[')
-    (char ']')
-    ( choice
-        [ Literal <$> L.decimal,
-          Packet <$> pPacket
-        ]
-        `sepBy` char ','
-    )
+instance Ord Packet where
+  compare :: Packet -> Packet -> Ordering
+  compare (Packet []) (Packet []) = EQ
+  compare (Packet []) _ = LT
+  compare _ (Packet []) = GT
+  compare (Literal a) (Literal b) = a `compare` b
+  compare lhs@(Packet _) rhs@(Literal _) = lhs `compare` Packet [rhs]
+  compare lhs@(Literal _) rhs@(Packet _) = Packet [lhs] `compare` rhs
+  compare (Packet (x : xs)) (Packet (y : ys)) =
+    case x `compare` y of
+      EQ -> Packet xs `compare` Packet ys
+      r -> r
 
-pPair :: Parser ([Packet], [Packet])
+pPacket :: Parser Packet
+pPacket =
+  Packet
+    <$> between
+      (char '[')
+      (char ']')
+      ( choice
+          [ Literal <$> L.decimal,
+            pPacket
+          ]
+          `sepBy` char ','
+      )
+
+pPair :: Parser (Packet, Packet)
 pPair = do
   lhs <- pPacket
   void newline
@@ -53,52 +67,36 @@ pPair = do
   void newline
   return (lhs, rhs)
 
-pInput :: Parser [([Packet], [Packet])]
+pInput :: Parser [(Packet, Packet)]
 pInput = pPair `sepBy` newline <* eof
 
-ordered :: [Packet] -> [Packet] -> Ordering
-ordered [] [] = EQ
-ordered [] _ = LT
-ordered _ [] = GT
-ordered (x : xs) (y : ys) =
-  case (x, y) of
-    (Literal a, Literal b) ->
-      if
-          | a < b -> LT
-          | a > b -> GT
-          | otherwise -> ordered xs ys
-    (Packet _, Literal b) -> ordered (x : xs) (Packet [Literal b] : ys)
-    (Literal a, Packet _) -> ordered (Packet [Literal a] : xs) (y : ys)
-    (Packet a, Packet b) ->
-      case ordered a b of
-        EQ -> ordered xs ys
-        r -> r
-
 part1 :: Input -> Result
-part1 ipt =
-  return $
-    sum $
-      catMaybes $
-        zipWith
-          (\(lhs, rhs) i -> if ordered lhs rhs == LT then Just i else Nothing)
-          ipt
-          [1 ..]
+part1 =
+  return
+    . sum
+    . catMaybes
+    . zipWith
+      (\i (lhs, rhs) -> if lhs < rhs then Just i else Nothing)
+      [1 ..]
 
-dividerPacket :: Int -> [Packet]
-dividerPacket n = [Packet [Packet [Literal n]]]
+dividerPacket :: Int -> Packet
+dividerPacket n = Packet [Packet [Literal n]]
 
-twoDivider :: [Packet]
+twoDivider :: Packet
 twoDivider = dividerPacket 2
 
-sixDivider :: [Packet]
+sixDivider :: Packet
 sixDivider = dividerPacket 6
 
 part2 :: Input -> Result
-part2 ipt = do
-  let packets = sortBy ordered $ twoDivider : sixDivider : concatMap (\(a, b) -> [a, b]) ipt
-  twoIndex <- elemIndex twoDivider packets
-  sixIndex <- elemIndex sixDivider packets
-  return $ (twoIndex + 1) * (sixIndex + 1)
+part2 ipt =
+  let packets =
+        sort $
+          twoDivider : sixDivider : concatMap (\(a, b) -> [a, b]) ipt
+   in do
+        twoIndex <- elemIndex twoDivider packets
+        sixIndex <- elemIndex sixDivider packets
+        return $ (twoIndex + 1) * (sixIndex + 1)
 
 solve' :: (Input -> Result) -> IO Text -> IO (ParserResult Result)
 solve' = solve3 pInput
